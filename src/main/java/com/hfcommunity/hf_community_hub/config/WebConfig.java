12d042eda5439a836b3c53370e7691527616eb87
Juan Fernando Jaramillo <tucorreo@ejemplo.com>
2025-06-02 09:57:52 -0500

    Refactor application.yml for improved database configuration and logging

--- src/main/java/com/hfcommunity/hf_community_hub/config/WebConfig.java
+++ src/main/java/com/hfcommunity/hf_community_hub/config/WebConfig.java
@@ -24,7 +24,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173", "https://front-community-hf-production.up.railway.app")
+                .allowedOrigins("https://front-community-hf-production.up.railway.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
