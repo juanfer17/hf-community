bf518a196d4be8c49cb7aff1100b3bf9f55f34f9
Juan Fernando Jaramillo <tucorreo@ejemplo.com>
2025-06-06 13:11:44 -0500

    Add modality management functionality with controller, service, and DTOs

--- src/main/java/com/hfcommunity/hf_community_hub/config/WebConfig.java
+++ src/main/java/com/hfcommunity/hf_community_hub/config/WebConfig.java
@@ -24,8 +24,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173")
-                //.allowedOrigins("https://front-community-hf-production.up.railway.app")
+                //.allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://front-community-hf-production.up.railway.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
